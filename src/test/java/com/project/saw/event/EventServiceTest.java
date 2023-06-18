28953c4f081623b3fe43003e34ccbe2a208d1ea5
AmmyB <amelia.90.b@gmail.com>
2023-06-18 23:55:57 +0200

    add EventService unit test

--- src/test/java/com/project/saw/event/EventServiceTest.java
+++ src/test/java/com/project/saw/event/EventServiceTest.java
@@ -1,41 +1,62 @@
 package com.project.saw.event;
 
+import com.project.saw.dto.CreateEventRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mockito;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.ArgumentMatchers.any;
+
 
 class EventServiceTest {
 
     private EventRepository eventRepository;
     private EventService eventService;
+    @Captor
+    ArgumentCaptor<EventEntity> eventEntityArgumentCaptor;
 
     @BeforeEach
     void setUp() {
+        eventEntityArgumentCaptor = ArgumentCaptor.forClass(EventEntity.class);
         eventRepository = Mockito.mock(EventRepository.class);
         eventService = new EventService(eventRepository);
     }
 
     @Test
-    public void test_getAll_not_with_empty_list() {
+    void given_not_empty_list_when_fetch_the_list_then_events_list_should_be_returned() {
         //given
-        List<EventEntity> list = Arrays.asList(new EventEntity(),new EventEntity());
+        List<EventEntity> list = Arrays.asList(new EventEntity(), new EventEntity());
         Mockito.when(eventRepository.findAll()).thenReturn(list);
         //when
         var results = eventService.getAll();
         //then
-        Assertions.assertEquals(list,results);
+        Assertions.assertEquals(list, results);
 
     }
 
     @Test
-    public void test_CreateEvent() {
-
-
+    void given_repo_with_not_existing_event_when_add_new_event_then_event_should_be_created() {
+        //given
+        CreateEventRequest request = new CreateEventRequest("test", "testLocation", 9.00, LocalDate.of(2001,1,2), LocalDate.of(2001,1,2),
+                "Test description");
+        //when
+        var results = eventService.createEvent(request);
+        //then
+        Mockito.verify(eventRepository, Mockito.times(1)).save(eventEntityArgumentCaptor.capture());
+        var capturedParameter = eventEntityArgumentCaptor.getValue();
+        Assertions.assertEquals(capturedParameter.getTitle(),request.getTitle());
+        Assertions.assertEquals(capturedParameter.getLocation(),request.getLocation());
+        Assertions.assertEquals(capturedParameter.getPrice(),request.getPrice());
+        Assertions.assertEquals(capturedParameter.getStartingDate(),request.getStartingDate());
+        Assertions.assertEquals(capturedParameter.getEndingDate(),request.getEndingDate());
+        Assertions.assertEquals(capturedParameter.getDescription(),request.getDescription());
     }
 }
