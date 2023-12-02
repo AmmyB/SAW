8e460048338dc522e3744da440b157b574309eaa
AmmyB <amelia.90.b@gmail.com>
2023-12-02 21:45:24 +0100

    Added UserService unit test - CreateUser

--- src/test/java/com/project/saw/user/UserServiceTest.java
+++ src/test/java/com/project/saw/user/UserServiceTest.java
@@ -1,9 +1,13 @@
 package com.project.saw.user;
 
+import com.project.saw.dto.user.CreateUserRequest;
 import com.project.saw.dto.user.UserProjections;
+import com.project.saw.exception.EmailExistsException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mockito;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.projection.ProjectionFactory;
@@ -12,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 import java.util.Collections;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 
 
 class UserServiceTest {
@@ -21,12 +28,15 @@ class UserServiceTest {
     private PasswordEncoder passwordEncoder;
     private UserService userService;
 
+    @Captor
+    ArgumentCaptor<UserEntity> userEntityArgumentCaptor;
 
     @BeforeEach
     void setUp() {
         userRepository = Mockito.mock(UserRepository.class);
         passwordEncoder = Mockito.mock(PasswordEncoder.class);
         userService = new UserService(userRepository, passwordEncoder);
+        userEntityArgumentCaptor = ArgumentCaptor.forClass(UserEntity.class);
 
     }
 
@@ -50,7 +60,22 @@ class UserServiceTest {
         var results = userService.getUserList();
 
 //        then
-        Assertions.assertEquals(Collections.singletonList(userProjections),results);
+        Assertions.assertEquals(Collections.singletonList(userProjections), results);
+    }
+
+    @Test
+    void given_repo_with_not_existing_user_when_add_new_user_then_user_should_be_created() throws EmailExistsException {
+        //given
+        CreateUserRequest request = new CreateUserRequest("unitTest", "xxx4", "xyz@nq.pl");
+        UserEntity response = UserEntity.builder()
+                .userName(request.userName()).build();
+        Mockito.when(userRepository.save(any())).thenReturn(response);
+        //when
+        var result = userService.createUser(request);
+        //then
+        Assertions.assertEquals(result.getUserName(), request.userName());
+
+
     }
 
 
